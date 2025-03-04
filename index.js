24be6b64b5be1f168470aff6e795d2e6211b3eae
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-04 15:42:25 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10) || 0;
+
+  if (left > 0) {
+    dodger.style.left = `${left - 5}px`; // Moves left by 5px
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px","");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`; 
+  }
+};
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
